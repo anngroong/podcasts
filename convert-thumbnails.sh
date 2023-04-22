48811daa6601b17a84196823dccd981e9d726319
Hovik Manucharyan <hovik.1337@gmail.com>
2023-04-22 21:48:33 +0400

    Adding file to convert thumbnails from png to jpg.

---
+++ convert-thumbnails.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script expects to be run from the root of the repository.
+# Converts all thumbnail-NNN.png files to thumbnail-NNN.jpg files.
+# It also edits the episode markdown file in place to change the reference in the file.
+
+modify_file () {
+  bn=$( basename $1 )
+  ep=$( echo $bn | sed -r 's/thumbnail-([0-9]*)\.png/\1/g' )
+  jpgfile="${bn%.png}.jpg"
+  for f in `find content/episode -name ${ep}-*.md`
+  do
+     echo "	Modifying $f"
+     gsed -i s/${bn}/${jpgfile}/g ${f}
+  done
+}
+
+for i in `find content/img/episode -name thumbnail-*.png`
+do
+    output="${i%.png}.jpg"
+    # Comment out the following IF statement to run across all episodes.
+    if [ -f "$output" ]; then
+       # Skipping for existing files
+       echo $output exists, skipping.
+       continue
+    fi
+
+    echo WORKING on $i
+    convert -quality 35 $i $output 2>/dev/null
+    ORIG_FILESIZE=$(stat -f%z "$i")
+    DEST_FILESIZE=$(stat -f%z "$output")
+    EDUCTION=$( printf %.2f $( echo "(1 - $DEST_FILESIZE/$ORIG_FILESIZE) * 100" | bc -l ) )   
+    echo "INPUT: $i ORIG: $ORIG_FILESIZE DEST: $DEST_FILESIZE REDUCTION: ${REDUCTION}%"
+    modify_file $i 
+done
+ 
