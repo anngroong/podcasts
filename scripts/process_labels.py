7331f3c4b4111552f0f6c896dc071ef73c7401ab
Hovik Manucharyan <hovik.1337@gmail.com>
2024-10-03 23:55:22 -0700

    Update process_labels to exclude hours if the timestamp is < 3600 secs

--- scripts/process_labels.py
+++ scripts/process_labels.py
@@ -10,9 +10,20 @@ parser.add_argument("-f", "--file", dest="filename", default="Labels 1.txt",
                     help="Path to Audacity labels file", metavar="FILE")
 args = parser.parse_args()
 
+def print_ts(secs, label, lt):
+  ts=str(datetime.timedelta(seconds=key))
+  if(lt):
+    ts=":".join(ts.split(":")[1:])
+  print("  {0} {1}".format(ts, timestamps[key]))
+
 print ("Timestamps:")
+timestamps = {}
 with open(args.filename) as tsv:
   for line in csv.reader(tsv, dialect="excel-tab"):
     secs=round(float(line[0]))
-    timestamp=str(datetime.timedelta(seconds=secs))
-    print("  {0} {1}".format(timestamp,line[2]))
+    timestamps[secs] = line[2]
+
+less_than_3600 = max(timestamps.keys()) < 3600
+
+for key in sorted(timestamps.keys()):
+  print_ts(key, timestamps[key], less_than_3600)
